3fba87ea9d06a5c3a8b4ec162ede95bfbf1de1e1
Vanaldito <fredimanuelb@gmail.com>
2022-10-08 16:09:22 -0600

    feat: add styles to Counter

--- src/components/Counter/Counter.tsx
+++ src/components/Counter/Counter.tsx
@@ -1,23 +1,34 @@
 import { useState } from "react";
 
+import "./Counter.css";
+
 export default function Counter() {
   const [counter, setCounter] = useState(0);
 
   return (
-    <div>
-      <div>{counter}</div>
-      <button type="button" onClick={() => setCounter(counter => counter + 1)}>
-        +
+    <div className="counter">
+      <div className="counter__amount">{counter}</div>
+      <button
+        className="counter__increment"
+        type="button"
+        onClick={() => setCounter(counter => counter + 1)}
+      >
+        <span>+</span>
       </button>
       <button
         type="button"
+        className="counter__decrement"
         onClick={() =>
           setCounter(counter => (counter - 1 < 0 ? 0 : counter - 1))
         }
       >
-        -
+        <span>-</span>
       </button>
-      <button type="button" onClick={() => setCounter(0)}>
+      <button
+        className="counter__delete"
+        type="button"
+        onClick={() => setCounter(0)}
+      >
         Delete
       </button>
     </div>
